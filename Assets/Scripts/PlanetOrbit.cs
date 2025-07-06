a8290615a53dcaf3a49fcbf5e2c743996696e510
eymen-44 <hacibekkoir@gmail.com>
2025-07-06 15:43:56 +0300

    Update PlanetOrbit.cs

--- Assets/Scripts/PlanetOrbit.cs
+++ Assets/Scripts/PlanetOrbit.cs
@@ -1,1 +1,10 @@
+using UnityEngine;
 
+public class CollisionHandler : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(gameObject.name + " collided with " + collision.gameObject.name);
+        // Buraya efekt veya ses ekleyebilirsin
+    }
+}
